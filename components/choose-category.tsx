cce83e29f73f60a3ff1a93f5ca2434f24c015600
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 16:36:53 -0400

    consolelog choose category

--- components/choose-category.tsx
+++ components/choose-category.tsx
@@ -18,6 +18,9 @@ const ChooseCategory = () => {
         return <p>No se encontraron categorías.</p>;
     }
 
+    // Imprimir los datos recibidos en la consola
+    console.log(result);
+
     return (
         <div className='max-w-6xl py-4 mx-auto sm:py-16 sm:px-24'>
             <h3 className='px-6 pb-4 text-3xl sm:pb-8 text-primary'>Elige tu categoria favorita</h3>
