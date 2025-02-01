2a5f9f8b4140f1ed5d18d6a1aa5cc7fb27897420
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 12:01:38 -0400

    cambios featured products

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -35,15 +35,15 @@ const FeaturedProducts = () => {
         <div className="max-w-6xl py-2 mx-auto sm:py-16 sm:px-24">
             <h3 className="px-6 text-3xl sm:pb-8 text-primary">Productos destacados</h3>
             <Carousel className="text-primary">
-                <CarouselContent className="-ml-2 md:-ml-4 ">
+                <CarouselContent className="-ml-2 md:-ml-4">
                     {result.map((product: ProductType) => {
                         const { id, slug, productName, images, category, estilo } = product;
-                        const imageUrl = images && images[0] ? images[0].url : null;
+                        const imageUrl = images && images[0] && images[0].url ? images[0].url : null;
 
                         return (
                             <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                                 <div className="p-1">
-                                    <Card className="py-4 border-primary/30 shadow-none ">
+                                    <Card className="py-4 border-primary/30 shadow-none">
                                         <CardContent className="relative flex items-center justify-center px-6 py-2">
                                             {imageUrl ? (
                                                 <img
@@ -64,7 +64,9 @@ const FeaturedProducts = () => {
                                             <h3 className="text-lg font-bold">{productName}</h3>
                                         </div>
                                         <div className="gap-4 px-7">
-                                            <ProductCategoryEstilo categoryName={category.categoryName} estilo={estilo} />
+                                            {category && category.categoryName && (
+                                                <ProductCategoryEstilo categoryName={category.categoryName} estilo={estilo} />
+                                            )}
                                         </div>
                                     </Card>
                                 </div>
