15fd511d479f60735e09ef2785cf7b6b4e1f2563
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 19:34:32 -0400

    feat: cambio carousel-products

--- app/(routes)/product/[productSlug]/components/carousel-product.tsx
+++ app/(routes)/product/[productSlug]/components/carousel-product.tsx
@@ -6,27 +6,37 @@ interface CarouselProductProps {
         id: number;
         url: string;
     }[];
-};
-
+}
 
 const CarouselProduct = (props: CarouselProductProps) => {
-    const { images } = props
-    //console.log(images)
+    const { images } = props;
 
     return (
         <div className="sm:px-16">
             <Carousel>
                 <CarouselContent>
-                    {images.map((image) => (
-                        <CarouselItem key={image.id}>
-                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`} 
-                            alt = "Image product" 
-                            className="rounded-lg" />
-                        </CarouselItem>
-                    ))}
+                    {images.length > 0 ? (
+                        images.map((image) => {
+                            const imageUrl = image.url.startsWith("http")
+                                ? image.url
+                                : `${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`;
+
+                            return (
+                                <CarouselItem key={image.id}>
+                                    <img
+                                        src={imageUrl}
+                                        alt="Product image"
+                                        className="rounded-lg"
+                                    />
+                                </CarouselItem>
+                            );
+                        })
+                    ) : (
+                        <p>No hay imágenes disponibles</p>
+                    )}
                 </CarouselContent>
                 <CarouselPrevious className="text-primary" />
-                <CarouselNext className="text-primary"/>
+                <CarouselNext className="text-primary" />
             </Carousel>
         </div>
     );
