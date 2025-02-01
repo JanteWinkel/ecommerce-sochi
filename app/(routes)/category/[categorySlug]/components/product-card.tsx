ca532bd367ae42e6ea89144817d1a18f2f210a3e
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 19:20:56 -0400

    cambios product-card

--- app/(routes)/category/[categorySlug]/components/product-card.tsx
+++ app/(routes)/category/[categorySlug]/components/product-card.tsx
@@ -11,7 +11,7 @@ import IconButton from "@/components/ui/icon-button";
 import { useCart } from "@/hooks/use-cart";
 
 type ProductCardProps = {
-    product: ProductType
+    product: ProductType;
 }
 
 const ProductCard = (props: ProductCardProps) => {
@@ -21,7 +21,6 @@ const ProductCard = (props: ProductCardProps) => {
 
     // Accede a los valores correctos según la estructura de product
     const images = product.attributes.images.data;
-    const imageUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0]?.attributes.url}`;
     const productName = product.attributes.productName;
     const price = product.attributes.price;
     const estilo = product.attributes.estilo;
@@ -40,23 +39,31 @@ const ProductCard = (props: ProductCardProps) => {
             </div>
             <Carousel opts={{ align: "start" }} className="w-full max-w-sm">
                 <CarouselContent>
-                    {images.map((image) => (
-                        <CarouselItem key={image.id} className="group">
-                            <div>
-                                <Link href={`/product/${product.attributes.slug}`}>
-                                    <img src={imageUrl}
-                                        alt={productName}
-                                        className="rounded-xl" />
-                                </Link>
-                            </div>
-                            <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
-                                <div className="flex justify-center gap-x-6">
-                                    <IconButton onClick={() => router.push(`/product/${product.attributes.slug}`)} icon={<Expand size={20} className="text-primary dark:text-primary" />} />
-                                    <IconButton onClick={() => addItem(product)} icon={<ShoppingCart size={20} className="text-primary dark:text-primary" />} />
+                    {images.map((image) => {
+                        const imageUrl = image.attributes.url.startsWith("http")
+                            ? image.attributes.url
+                            : `${process.env.NEXT_PUBLIC_BACKEND_URL}${image.attributes.url}`;
+
+                        return (
+                            <CarouselItem key={image.id} className="group">
+                                <div>
+                                    <Link href={`/product/${product.attributes.slug}`}>
+                                        <img
+                                            src={imageUrl}
+                                            alt={productName}
+                                            className="rounded-xl"
+                                        />
+                                    </Link>
                                 </div>
-                            </div>
-                        </CarouselItem>
-                    ))}
+                                <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                                    <div className="flex justify-center gap-x-6">
+                                        <IconButton onClick={() => router.push(`/product/${product.attributes.slug}`)} icon={<Expand size={20} className="text-primary dark:text-primary" />} />
+                                        <IconButton onClick={() => addItem(product)} icon={<ShoppingCart size={20} className="text-primary dark:text-primary" />} />
+                                    </div>
+                                </div>
+                            </CarouselItem>
+                        );
+                    })}
                 </CarouselContent>
             </Carousel>
             <p className="font-bold text-center">{productName}</p>
