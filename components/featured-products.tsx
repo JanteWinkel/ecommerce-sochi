71e488aedea13af9e214599b8b8016b5f7a78034
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 18:33:42 -0400

    cambios featured products nextconfig

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -35,8 +35,6 @@ const FeaturedProducts = () => {
         return <p>No se encontraron productos destacados.</p>;
     }
 
-    console.log("Productos destacados:", result);
-
     return (
         <div className="max-w-6xl py-2 mx-auto sm:py-16 sm:px-24">
             <h3 className="px-6 text-3xl sm:pb-8 text-primary">Productos destacados</h3>
@@ -52,24 +50,18 @@ const FeaturedProducts = () => {
                             : "/path/to/default-image.jpg"; // Imagen por defecto si no hay imagen
                         const categoryName = category?.data?.attributes?.categoryName ?? "Sin categoría";
 
-                        console.log(`Producto: ${productName}, Imagen URL: ${imageUrl}`);
-
                         return (
                             <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                                 <div className="p-1">
                                     <Card className="py-4 border-primary/30 shadow-none">
                                         <CardContent className="relative flex items-center justify-center px-6 py-2">
-                                            {imageUrl ? (
-                                                <Image 
-                                                    src={imageUrl} 
-                                                    alt={productName} 
-                                                    width={270} 
-                                                    height={270} 
-                                                    className="rounded-lg"
-                                                />
-                                            ) : (
-                                                <p>Imagen no disponible</p>
-                                            )}
+                                            <Image 
+                                                src={imageUrl} 
+                                                alt={productName} 
+                                                width={270} 
+                                                height={270} 
+                                                className="rounded-lg"
+                                            />
                                             <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                                                 <div className="flex justify-center gap-x-6">
                                                     <IconButton 
