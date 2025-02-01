fc2f2e5807e236c4209739398124549ce2e846d8
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 12:04:15 -0400

    cambios choose category

--- components/choose-category.tsx
+++ components/choose-category.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
+"use client";
 
 import { ResponseType } from "@/types/response";
 import { useGetCategory } from "@/api/getProducts";
@@ -7,8 +7,7 @@ import Link from "next/link";
 import { CategoryType } from "@/types/category";
 
 const ChooseCategory = () => {
-
-    const { result, loading }: ResponseType = useGetCategory()
+    const { result, loading }: ResponseType = useGetCategory();
 
     // Verificación de que `result` es un array y no es null ni undefined
     if (loading) {
@@ -24,25 +23,31 @@ const ChooseCategory = () => {
             <h3 className='px-6 pb-4 text-3xl sm:pb-8 text-primary'>Elige tu categoria favorita</h3>
 
             <div className='grid gap-5 sm:grid-cols-3'>
-                {result.map((category: CategoryType) => (
-                    <Link
-                        key={category.id}
-                        href={`/category/${category.slug}`}
-                        className='relative max-w-xs mx-auto overflow-hidden bg-no-repeat bg-cover rounded-lg'
-                    >
-                        <img 
-                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${category.mainImage.url}`} 
-                            alt={category.categoryName} 
-                            className="max-w-[270px] transition duration-300 ease-in-out rounded-lg hover:scale-110"
-                        />
-                        <p className="absolute w-full py-2 text-lg font-bold text-center text-black bottom-5 backdrop-blur-lg">
-                            {category.categoryName}
-                        </p>
-                    </Link>
-                ))}
+                {result.map((category: CategoryType) => {
+                    const imageUrl = category.mainImage && category.mainImage.url ? category.mainImage.url : null;
+                    return (
+                        <Link
+                            key={category.id}
+                            href={`/category/${category.slug}`}
+                            className='relative max-w-xs mx-auto overflow-hidden bg-no-repeat bg-cover rounded-lg'
+                        >
+                            {imageUrl ? (
+                                <img 
+                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${imageUrl}`} 
+                                    alt={category.categoryName} 
+                                    className="max-w-[270px] transition duration-300 ease-in-out rounded-lg hover:scale-110"
+                                />
+                            ) : (
+                                <p>Imagen no disponible</p>
+                            )}
+                            <p className="absolute w-full py-2 text-lg font-bold text-center text-black bottom-5 backdrop-blur-lg">
+                                {category.categoryName}
+                            </p>
+                        </Link>
+                    );
+                })}
             </div>
-
         </div>
     );
-}
+};
 export default ChooseCategory;
