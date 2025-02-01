c662234cb86cae6a04a09d21a39abddc6ffeeb0e
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 19:14:23 -0400

    cambios product-image-miniature

--- app/product-images-miniature.tsx
+++ app/product-images-miniature.tsx
@@ -6,18 +6,23 @@ interface ProductImageMiniatureProps {
    slug: string,
    url: string
 }
+
 const ProductImageMiniature = (props: ProductImageMiniatureProps) => {
-    const { slug, url } = props
-    const router = useRouter()
+    const { slug, url } = props;
+    const router = useRouter();
 
-    return ( 
+    // Verifica si la URL ya es completa o necesita ser concatenada con NEXT_PUBLIC_BACKEND_URL
+    const imageUrl = url.startsWith("http") ? url : `${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`;
+
+    return (
         <div onClick={() => router.push(`/product/${slug}`)} className="cursor-pointer">
-                <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${url}`}
-                    alt="Product"
-                    className="w-24 h-24 overflow-hidden rounded-md sm:w-auto sm:h-32" />
-
-            </div>
-     );
+            <img
+                src={imageUrl}
+                alt="Product"
+                className="w-24 h-24 overflow-hidden rounded-md sm:w-auto sm:h-32"
+            />
+        </div>
+    );
 }
- 
+
 export default ProductImageMiniature;
