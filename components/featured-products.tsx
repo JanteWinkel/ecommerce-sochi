21282510731bacf6a8ed9d711be5dc5c855998ec
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 16:33:21 -0400

    feat: featured products

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -35,19 +35,24 @@ const FeaturedProducts = () => {
         <div className="max-w-6xl py-2 mx-auto sm:py-16 sm:px-24">
             <h3 className="px-6 text-3xl sm:pb-8 text-primary">Productos destacados</h3>
             <Carousel className="text-primary">
-                <CarouselContent className="-ml-2 md:-ml-4 ">
+                <CarouselContent className="-ml-2 md:-ml-4">
                     {result.map((product: ProductType) => {
-                        const { id, slug, productName } = product;
+                        const { id, slug, productName, images, category, estilo } = product;
+                        const imageUrl = images && images[0] && images[0].url ? images[0].url : null;
 
                         return (
                             <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                                 <div className="p-1">
-                                    <Card className="py-4 border-primary/30 shadow-none ">
+                                    <Card className="py-4 border-primary/30 shadow-none">
                                         <CardContent className="relative flex items-center justify-center px-6 py-2">
-                                            <img
-                                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${product.images[0].url}`}
-                                                alt="Image featured"
-                                            />
+                                            {imageUrl ? (
+                                                <img
+                                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${imageUrl}`}
+                                                    alt="Image featured"
+                                                />
+                                            ) : (
+                                                <p>Imagen no disponible</p>
+                                            )}
                                             <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                                                 <div className="flex justify-center gap-x-6">
                                                     <IconButton onClick={() => router.push(`product/${slug}`)} icon={<Expand size={20} className="text-primary dark:text-primary" />} />
@@ -59,7 +64,9 @@ const FeaturedProducts = () => {
                                             <h3 className="text-lg font-bold">{productName}</h3>
                                         </div>
                                         <div className="gap-4 px-7">
-                                            <ProductCategoryEstilo categoryName={product.category.categoryName} estilo={product.estilo} />
+                                            {category && category.categoryName && (
+                                                <ProductCategoryEstilo categoryName={category.categoryName} estilo={estilo} />
+                                            )}
                                         </div>
                                     </Card>
                                 </div>
@@ -72,6 +79,6 @@ const FeaturedProducts = () => {
             </Carousel>
         </div>
     );
-}
+};
 
 export default FeaturedProducts;
